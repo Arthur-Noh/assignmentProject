b87973a8047c0a980922728c14fbffeef7f76d35
Arthur-Noh <dkagod97@gmail.com>
2022-12-31 09:45:46 +0900

    feat(page): issueList

--- interface/types.ts
+++ interface/types.ts
@@ -1,9 +1,13 @@
 // TODO: 정의하지 못한 DTO 필요할 때마다 추가 작성
 
+export interface UserDTO {
+    login: string;
+}
+
 export type IssueListDTO = {
     number: number;
     title: string;
-    user: string;
+    user: UserDTO;
     created_at: string;
     comments: number;
 }
--- pages/issueList/index.tsx
+++ pages/issueList/index.tsx
@@ -1,8 +1,13 @@
-import { useNavigation } from '@react-navigation/native';
+import { Link, useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React, { useContext, useEffect } from 'react';
+import { Linking, Pressable } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
+import IssueCard from '../../components/atoms/issueCard';
+import { scaler } from '../../helper/scaler';
 import useCustomHeader from '../../hooks/useCustomHeader';
+import { IssueListDTO } from '../../interface/types';
 import { IssueContext } from '../../providers/issueProvider';
 import { AppStackParamList } from '../../route/routeDef';
 import repositoryService from '../../services/repositoryService';
@@ -10,17 +15,19 @@ import { DEFAULT_PAGE, DEFAULT_PER_PAGE } from './constant';
 
 const Layout = styled.View`
 flex: 1;
+padding: 0 ${({ theme }) => theme.base.templatePadding}px;
 `;
 
-const DefaultText = styled.Text`
-font-size: ${({ theme }) =>  theme.typography.size.m1}px;
+const AdBox = styled.Image`
+width: 100%;
+height: ${scaler(60)}px;
+margin: ${scaler(5)}px 0;
 `;
 
-const NaviButton = styled.Pressable`
-width: 50px;
-height: 50px;
-background-color: ${({ theme }) => theme.colors.primary.red};
-`;
+type IssueCardType = {
+    item: IssueListDTO,
+    index: number,
+};
 
 const IssueList = () => {
     const navigation = useNavigation<NativeStackNavigationProp<AppStackParamList, 'IssueList'>>();
@@ -30,12 +37,39 @@ const IssueList = () => {
         try {
             const response = await repositoryService.getIssues(page, perPage);
             if (response) {
-                // console.log(response);
                 issueContext.setIssueList(response);
             }
         } catch (error) {
             console.log('IssueList initialize error =>', error);
         }
+    }
+
+    const renderIssueCard = (data: IssueCardType) => {
+        const { item, index } = data;
+
+        return (
+            <>
+            <IssueCard
+                key={`card${index}`}
+                title={item.title}
+                name={item.user.login}
+                issueNumber={item.number}
+                createDate={item.created_at}
+                comments={item.comments}
+                onPress={() => navigation.navigate('IssueDetail')}
+            />
+            { index === 4 && (
+                <Pressable
+                    key={`ad${index}`}
+                    onPress={async () => await Linking.openURL('https://thingsflow.com/ko/home')}
+                >
+                    <AdBox 
+                        source={{ uri : 'https://hellobot-test.s3.ap-northeast-2.amazonaws.com/image/01fdd797-0477-4717-8d70-8551150463f7' }}
+                    />
+                </Pressable>
+            )}
+            </>
+        );
     }
 
     useCustomHeader({ title: 'Angular/Angular-cli' });
@@ -46,8 +80,12 @@ const IssueList = () => {
 
     return (
         <Layout>
-            <DefaultText>이슈 리스트 페이지</DefaultText>
-            <NaviButton onPress={() => navigation.navigate('IssueDetail')}/>
+            <FlatList
+                data={issueContext.issueList}
+                renderItem={renderIssueCard}
+                keyExtractor={(item) => `${item.number}-${item.title}`}
+                contentContainerStyle={{ backgroundColor: 'white' }}
+            />
         </Layout>
     );
 };
